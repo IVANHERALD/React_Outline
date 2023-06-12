a062a606c352c62ed287454058d663bb6c2a5dfc
Ivan herald <ivanheraldd@gmail.com>
2023-06-12 19:47:39 +0530

    Grid

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -4,6 +4,7 @@ import { Search } from 'semantic-ui-react'
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import { useNavigate } from 'react-router-dom';
 import './Profile.css';
+import { Grid } from '@mui/material';
 function Profile() {
   const history=useNavigate();
   const handleClick=()=>{
@@ -13,15 +14,31 @@ function Profile() {
     <div>
       <Search/><br/><br></br>
       <div className='Categories'>
+        <Grid container spacing={3} direction="row"
+        justifyContent="center"
+        alignItems="center"
+        >
+          <Grid item md={4}>
+            
         <div className='Category-1'>
          <CameraAltIcon sx={{fontSize:80}} onClick={handleClick}/>
         </div>
+      
+        </Grid>
+      
+          <Grid item md={4}>
+            
         <div className='Category-2'>
          <CameraAltIcon sx={{fontSize:80}}/>
         </div>
+      
+        </Grid>
+        <Grid item md={4}>
         <div className='Category-3'>
          <CameraAltIcon sx={{fontSize:80}}/>
         </div>
+        </Grid>
+        </Grid>
       </div>
       <Footer/>
     </div>
